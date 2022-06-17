9b70b91953ca0ec473c41de25a99532dd7e8e972
rovert19 <eduardororo285@gmail.com>
2022-06-17 17:34:35 -0500

    Se agrego el package json al ignore

--- horario-version-1/index.js
+++ horario-version-1/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import { checkConsecutivos } from "../filtros/ciclos-consecutivos"
 import { ordenarHoras } from "./filtros/ordenarPorHora"
 import { getTodasHoras, getInfoCursos } from "./fetchs/infoCursos"
